443efe922b474ab21067eeeb1b9789f110f6e157
1102581553 <pheyeji@gmail.com>
2026-02-26 13:59:16 +0000

    Update Config.h

--- src/Config.h
+++ src/Config.h
@@ -4,12 +4,12 @@
 namespace parallel_tick {
 
 struct Config {
-    int  version           = 1;
-    bool enabled           = true;
-    bool debug             = false; // 新增：Debug 开关
-    int  batchSize         = 64;   
-    float gridSize         = 16.0f; 
-    bool parallelItemsOnly = false; 
+    int   version           = 1;
+    bool  enabled           = true;
+    bool  debug             = false;
+    int   batchSize         = 64;
+    float gridSize          = 16.0f;
+    bool  parallelItemsOnly = false;
 };
 
 } // namespace parallel_tick
