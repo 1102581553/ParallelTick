1346a8d59ba4251b7b102ca5ff523b1a137393dc
1102581553 <pheyeji@gmail.com>
2026-02-27 12:45:25 +0000

    Update Config.h

--- src/Config.h
+++ src/Config.h
@@ -1,15 +1,17 @@
 #pragma once
-#include <ll/api/data/JsonSerializable.h>
 
-struct Config : ll::data::JsonSerializable<Config> {
+struct Config {
     bool  enabled                = true;
     bool  debug                  = false;
     bool  stats                  = true;
     int   threadCount            = 0;
+
+    float gridSizeBase           = 64.0f;
+
     int   maxEntitiesPerTask     = 256;
     int   minEntitiesPerTask     = 8;
     int   maxEntitiesPerTaskLimit= 1024;
-    float gridSizeBase           = 16.0f;
+
     bool  autoAdjust             = true;
     int   targetTickMs           = 40;
     int   adjustStep             = 10;
