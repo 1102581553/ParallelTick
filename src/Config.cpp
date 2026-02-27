c6a86b53ed514ede35b2346b70b52461fd41c52e
1102581553 <pheyeji@gmail.com>
2026-02-27 16:55:16 +0000

    Update Config.cpp

--- src/Config.cpp
+++ src/Config.cpp
@@ -1,7 +1,4 @@
 #include "Config.h"
-#include <ll/api/Config.h>
-#include <ll/api/io/Logger.h>
-#include <ll/api/mod/NativeMod.h>
 
 namespace parallel_tick {
 
@@ -9,16 +6,6 @@ static Config configInstance;
 
 Config& getConfig() {
     return configInstance;
-}
-
-bool loadConfig() {
-    auto configPath = ll::mod::NativeMod::current()->getConfigDir() / "parallel_tick.json";
-    return ll::config::loadConfig(configInstance, configPath);
-}
-
-bool saveConfig() {
-    auto configPath = ll::mod::NativeMod::current()->getConfigDir() / "parallel_tick.json";
-    return ll::config::saveConfig(configInstance, configPath);
 }
 
 } // namespace parallel_tick
