35cf332d6a96cf457c09fa65816790e3a1744e05
1102581553 <pheyeji@gmail.com>
2026-02-27 16:45:09 +0000

    Update Config.cpp

--- src/Config.cpp
+++ src/Config.cpp
@@ -2,7 +2,6 @@
 #include <ll/api/Config.h>
 #include <ll/api/io/Logger.h>
 #include <ll/api/mod/NativeMod.h>
-#include <filesystem>
 
 namespace parallel_tick {
 
@@ -13,33 +12,8 @@ Config& getConfig() {
 }
 
 bool loadConfig() {
-    auto& logger = ll::mod::NativeMod::current()->getLogger();
-    auto configDir = ll::mod::NativeMod::current()->getConfigDir();
-    auto configPath = configDir / "parallel_tick.json";
-
-    try {
-        if (!ll::config::loadConfig(configInstance, configPath)) {
-            // 文件不存在，创建默认配置
-            if (!std::filesystem::exists(configDir)) {
-                std::filesystem::create_directories(configDir);
-            }
-            if (!ll::config::saveConfig(configInstance, configPath)) {
-                logger.error("Failed to save default config to {}", configPath.string());
-                return false;
-            }
-            logger.info("Created default config at {}", configPath.string());
-        } else {
-            logger.info("Loaded config from {}", configPath.string());
-        }
-        // 版本检查
-        if (configInstance.version != 2) {
-            logger.warn("Config version mismatch, some settings may be reset.");
-        }
-        return true;
-    } catch (const std::exception& e) {
-        logger.error("Failed to load config: {}", e.what());
-        return false;
-    }
+    auto configPath = ll::mod::NativeMod::current()->getConfigDir() / "parallel_tick.json";
+    return ll::config::loadConfig(configInstance, configPath);
 }
 
 bool saveConfig() {
